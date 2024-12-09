ee8f09e767c79c13c602b69d9bba7989ebd3ee7b
b0xf <librabfa@hotmail.com>
2024-12-09 07:58:01 +0300

    Report_form.php

--- Report_form.php
+++ Report_form.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Analytics Filter</title>
+    <title>Report Filter</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <main class="container">
-        <h1>Filter Analytics</h1>
-        <form method="GET" action="Analytics.php">
+        <h1>Filter Report</h1>
+        <form method="GET" action="report.php">
             <label for="roomFilter">Room Name:</label>
             <input type="text" id="roomFilter" name="roomFilter" placeholder="Enter room name">
 
@@ -25,9 +25,9 @@
         <nav>
             <ul>
                 <li><a href="Dashboard.php">Dashboard</a></li>
-                <li><a href="Analytics.php">Analytics</a></li>
+                <li><a href="report.php">Report</a></li>
             </ul> 
-            \\
+            
         </nav>
     </main>
 </body>
